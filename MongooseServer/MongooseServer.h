a5f18e6453e901d28071c5dcf2bf69025ab0e7ce
yangjs <yangjingsheng@qingting.fm>
2017-01-02 10:02:12 +0800

    init xcode proj

---
+++ MongooseServer/MongooseServer.h
@@ -0,0 +1,19 @@
+//
+//  MongooseServer.h
+//  MongooseServer
+//
+//  Created by Zack Yang on 2017/1/2.
+//  Copyright © 2017年 slepox Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MongooseServer.
+FOUNDATION_EXPORT double MongooseServerVersionNumber;
+
+//! Project version string for MongooseServer.
+FOUNDATION_EXPORT const unsigned char MongooseServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MongooseServer/PublicHeader.h>
+
+
