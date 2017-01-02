5140fd672259c5c323925e8854bfd267c321f053
yangjs <yangjingsheng@qingting.fm>
2017-01-02 10:22:20 +0800

    use ARC in proj

--- MongooseServer/MongooseServer.m
+++ MongooseServer/MongooseServer.m
@@ -32,73 +32,72 @@ void *handleRequest(enum mg_event event,
     
     // Reads the body of the request
     
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    
-    NSData *body = nil;
-    
-    if ((cl = mg_get_header(conn, "Content-Length")) != NULL) {
+    @autoreleasepool {
         
-        len = atoi(cl);
-        if ((buf = malloc(len)) != NULL) {
-            mg_read(conn, buf, len);
-            body = [NSData dataWithBytes:buf length:len];
-            free(buf);
-        }
-    }
-    
-    NSString *path = [NSString stringWithUTF8String:request_info->uri];
-    
-    // Search for a servlet to respond to the exact path
-    
-    Servlet *servlet = [server.servlets valueForKey:path];
-    
-    // If an exact match is not found, tries to match wildcard servlets
-    if (!servlet) {
-        for (NSString *servletPath in server.servlets) {
+        NSData *body = nil;
+        
+        if ((cl = mg_get_header(conn, "Content-Length")) != NULL) {
             
-            NSPredicate *pred = [NSPredicate predicateWithFormat:@"self LIKE %@", servletPath];
-            if ([pred evaluateWithObject:path]) {
-                servlet = [server.servlets valueForKey:servletPath];
+            len = atoi(cl);
+            if ((buf = malloc(len)) != NULL) {
+                mg_read(conn, buf, len);
+                body = [NSData dataWithBytes:buf length:len];
+                free(buf);
             }
         }
-    }
-    
-    ServletResponse *response = nil;
-    
-    if (servlet) {
         
-        ServletRequest *request = [[ServletRequest alloc] initWithRequestInfo:request_info body:body];
+        NSString *path = [NSString stringWithUTF8String:request_info->uri];
         
-        if (strcmp(request_info->request_method, "GET") == 0) {
-            response = [servlet doGet:request];
-        } else if (strcmp(request_info->request_method, "POST") == 0) {
-            response = [servlet doPost:request];
+        // Search for a servlet to respond to the exact path
+        
+        Servlet *servlet = [server.servlets valueForKey:path];
+        
+        // If an exact match is not found, tries to match wildcard servlets
+        if (!servlet) {
+            for (NSString *servletPath in server.servlets) {
+                
+                NSPredicate *pred = [NSPredicate predicateWithFormat:@"self LIKE %@", servletPath];
+                if ([pred evaluateWithObject:path]) {
+                    servlet = [server.servlets valueForKey:servletPath];
+                }
+            }
         }
         
-    } else {
+        ServletResponse *response = nil;
         
-        // If directory listing is enabled and no servlet was found, let
-        // Mongoose handle it
-        
-        if (strcmp(mg_get_option(server.ctx, "enable_directory_listing"), "yes") == 0) {
+        if (servlet) {
             
-            return NULL;
+            ServletRequest *request = [[ServletRequest alloc] initWithRequestInfo:request_info body:body];
+            
+            if (strcmp(request_info->request_method, "GET") == 0) {
+                response = [servlet doGet:request];
+            } else if (strcmp(request_info->request_method, "POST") == 0) {
+                response = [servlet doPost:request];
+            }
             
         } else {
             
-            // If no servlets were found to respond, sends a 404 error
+            // If directory listing is enabled and no servlet was found, let
+            // Mongoose handle it
             
-            servlet = [[[Servlet alloc] init] autorelease];
-            response = [servlet sendNotFound];
+            if (strcmp(mg_get_option(server.ctx, "enable_directory_listing"), "yes") == 0) {
+                
+                return NULL;
+                
+            } else {
+                
+                // If no servlets were found to respond, sends a 404 error
+                servlet = [[Servlet alloc] init];
+                response = [servlet sendNotFound];
+            }
+            
         }
-
+        
+        NSData *resp = [response toBinary];
+        
+        mg_write(conn, [resp bytes], [resp length]);
+        
     }
-    
-    NSData *resp = [response toBinary];
-    
-    mg_write(conn, [resp bytes], [resp length]);
-    
-    [pool release];
     
     return "handled";
 }
@@ -116,7 +115,7 @@ void *handleRequest(enum mg_event event,
             NULL
         };
         
-        ctx = mg_start(handleRequest, self, options);
+        ctx = mg_start(handleRequest, (__bridge void *)(self), options);
         
         servlets = [[NSMutableDictionary alloc] init];
         
@@ -128,20 +127,17 @@ void *handleRequest(enum mg_event event,
     
     if ((self = [super init])) {
         
-        ctx = mg_start(handleRequest, self, options);
+        ctx = mg_start(handleRequest, (__bridge void *)(self), options);
         
         servlets = [[NSMutableDictionary alloc] init];
         
     }
     return self;
-
+    
 }
 
 - (void)dealloc {
-    
     mg_stop(ctx);
-    [servlets release];
-    [super dealloc];
 }
 
 - (void)addServlet:(Servlet *)servlet forPath:(NSString *)path {
